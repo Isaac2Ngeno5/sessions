9051c704daae98f332fbbdb187171e5e33f7132f
Isaac2Ngeno5 <isaac2ngeno5@gmail.com>
2018-02-22 10:13:39 +0300

    added sql file

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-#Learning Php sessions.
+# Learning Php sessions.
 this is a three page php code .
-The first page i start the session by clalling the session start function,
+The first page i start the session by calling the session start function,
 i also set the session variables.
 The second page (demo_session2.php) the session start function is still called and then i desplay the session array contiaining the session variables
 The third page destroy.php start session then call the session unset function to unset the session array and the session destroy function to end session
+
+## php sessions 2.0
+Created a login page which is linked to a database
+session variable is set on login using the user id
+ i never knew sessions were this easy
+
+ Happy coding
---
+++ users.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 22, 2018 at 09:11 AM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sessions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'script', 'script@gmail.com', '1234'),
+(2, 'viola', 'viola@gmail.com', '1234'),
+(3, 'camila', 'camila@gmail.com', '1234'),
+(4, 'ed', 'ed@gmail.com', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
